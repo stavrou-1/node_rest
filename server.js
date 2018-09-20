9dac24b04273a015847089f1d696cae926116878
Yiannis Stavrou <yiannis@yiannis2.lan>
2018-09-20 11:21:24 -0500

    add deployment port

--- server.js
+++ server.js
@@ -5,9 +5,10 @@ const app = express();
 const mongoose = require('mongoose');
 const Task = require('./api/models/todoListModel'); //created model loading here
 const bodyParser = require('body-parser');
+const port = process.env.PORT || 4040;
 
 // mongoose instance connection url connection
-// mongoose.Promise = global.Promise;
+mongoose.Promise = global.Promise;
 mongoose.connect('mongodb://127.0.0.1:27017/todoListApi', { keepAlive: true, keepAliveInitialDelay: 300000, useNewUrlParser: true }); // this is our local testing environment
 
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -18,7 +19,7 @@ routes(app); //register the route
 
 console.log('todo list RESTful API server started..');
 
-app.listen(4040);
+app.listen(port);
 
 app.use(function(req, res) {
   res.status(404).send({url: req.originalUrl + ' not found'})
