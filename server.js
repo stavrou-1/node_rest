34804c59ee747f016d619cff24f1d59f9a59c2da
Yiannis Stavrou <yiannis@yiannis2.lan>
2018-09-20 11:18:58 -0500

    remove global promise

--- server.js
+++ server.js
@@ -7,13 +7,11 @@ const Task = require('./api/models/todoListModel'); //created model loading here
 const bodyParser = require('body-parser');
 
 // mongoose instance connection url connection
-mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://127.0.0.1:27017/todoListApi', { keepAlive: true, keepAliveInitialDelay: 300000, useNewUrlParser: true });
-
+// mongoose.Promise = global.Promise;
+mongoose.connect('mongodb://127.0.0.1:27017/todoListApi', { keepAlive: true, keepAliveInitialDelay: 300000, useNewUrlParser: true }); // this is our local testing environment
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
-
 
 var routes = require('./api/routes/todoListRoutes'); //importing route
 routes(app); //register the route
