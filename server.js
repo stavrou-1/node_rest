d68ff1d7fa12ad68abb2592cac988c49c9300ce7
Yiannis Stavrou <yiannis@yiannis2.local>
2018-10-13 10:54:48 -0500

    Add OPTIONS authentication

--- server.js
+++ server.js
@@ -36,6 +36,23 @@ app.use(function (req, res, next) {
 
   // Pass to next layer of middleware
   next();
+
+  if (req.method === 'OPTIONS') {
+    console.log('!OPTIONS');
+    var headers = {};
+
+    // IE8 does not allow domains to be specific, just the *
+    // headers['Access-Control-Allow-Origin'] = req.headers.origin
+    headers["Access-Control-Allow-Origin"] = "*";
+    headers["Access-Control-Allow-Methods"] = "POST, GET, PUT, DELETE, OPTIONS";
+    headers["Access-Control-Allow-Credentials"] = false;
+    headers["Access-Control-Max-Age"] = '86400'; // 24 hours
+    headers["Access-Control-Allow-Headers"] = "X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept";
+    res.writeHead(200, headers);
+    res.end();
+  } else {
+    // other requests.
+  }
 });
 
 var db = mongoose.connection;
